5d2be57fbdf23ca31005e6877205eeecbdd20e30
jianhanke <1340289015@qq.com>
2019-09-10 17:01:34 +0800

    NoVNCController.class.php/ modify hostname

--- Experiment/Application/Home/Controller/NoVNCController.class.php
+++ Experiment/Application/Home/Controller/NoVNCController.class.php
@@ -7,7 +7,10 @@ class NoVNCController extends MyController{
 
 	public function showNoVNC($ip_num){
 		// dump($ip_num);
-		$url='ws://localhost:6080/websockify?token=host'.$ip_num;
+		$noVNC=new \Home\Controller\Src\Host();
+		$hostName=$noVNC->getHostName();
+
+		$url='ws://'.$hostName.':6080/websockify?token=host'.$ip_num;
 		$this->assign('url',$url);
 		$this->display('NoVNC/showNoVNC');
 	}
